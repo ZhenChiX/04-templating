2125d97f1137a2e9290fbb7c99310022fba432ff
StarGale <zhenchix@hotmail.com>
2018-06-08 13:23:25 -0700

    nothing change

--- chi-paul/scripts/articleView.js
+++ chi-paul/scripts/articleView.js
@@ -11,6 +11,7 @@ articleView.populateFilters = function() {
   $('article').each(function() {
     if (!$(this).hasClass('template')) {
       let val = $(this).find('address a').text();
+      let val =$(this).attr('data-author');
       let optionTag = `<option value="${val}">${val}</option>`;
 
       if ($(`#author-filter option[value="${val}"]`).length === 0) {
